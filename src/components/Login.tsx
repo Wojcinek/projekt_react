65fb2ac69028c501b1616c9615386cf3d696cf8a
wojci <wojci@DESKTOP-PK2VBRC>
2024-01-09 11:27:46 +0100

    login and logout

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,24 +11,54 @@ const Login = () => {
     const dispatch = useDispatch<AppDispatch>()
     const handleLogin = (event: React.FormEvent) => {
         event.preventDefault()
-        if(users.find(user => user.email === email) !== undefined) {
-            dispatch(login(email))
+        let user = users.find(user => user.email === email)
+        if(user!== undefined) {
+            dispatch(login(user))
             return
         }
         console.log("User not found")
     }
 
-    return (<div className="login">
-        <form className="login-form" onSubmit={event => handleLogin(event)}>
-            <h1>Login</h1>
-            <input
-                placeholder={"Name"}
-                value={email}
+    return (
+        
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-300 dark:border-gray-500'>
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in
+        </h2>
+        </div>
+        
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={event => handleLogin(event)}>
+            <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 text-center">
+                Email address
+            </label>
+            <div className="mt-2">
+                <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
                 onChange={(i) => setEmail(i.target.value)}
-            />
-            <button className={"login-submit"}>Login</button>
+                className="block w-full justify-center px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-400 rounded-lg bg-gray-100"
+                />
+            </div>
+            </div>
+
+            <div>
+            <button
+                type="submit"
+                className="flex w-full justify-center bg-lime-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-lime-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600 mb-5 rounded-lg"
+            >
+                Log in
+            </button>
+            </div>
         </form>
-    </div>);
-};
+        </div>
+    </div>
+    </div>
+    )};
 
 export default Login;
--- src/features/UsersSlice.ts
+++ src/features/UsersSlice.ts
@@ -1,28 +1,28 @@
 import { User } from './../model/User';
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export interface UsersState {
-    users: User[]
+    users: User[];
 }
 
 export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const data = await response.json()
-    return data
-})
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    return data;
+});
 
 export const usersSlice = createSlice({
     name: "users",
     initialState: {
-        users: []
+        users: [],
     },
     reducers: {},
     extraReducers: {
         [fetchUsers.fulfilled.type]: (state, action) => {
-            state.users = action.payload
-        }
-    }
-})
+            state.users = action.payload;
+        },
+    },
+});
 
-export default usersSlice.reducer
-export const selectUsers = (state: {users: UsersState}) => state.users.users
+export default usersSlice.reducer;
+export const selectUsers = (state: {users: UsersState}) => state.users.users;
